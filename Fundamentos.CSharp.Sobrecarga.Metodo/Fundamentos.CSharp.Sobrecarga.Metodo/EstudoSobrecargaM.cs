5e840a0c12052a32bb8b892782b50609da635e60
BrunaBaria <136763236+BrunaBaria@users.noreply.github.com>
2025-09-18 17:37:32 -0300

    Add files via upload

---
+++ Fundamentos.CSharp.Sobrecarga.Metodo/Fundamentos.CSharp.Sobrecarga.Metodo/EstudoSobrecargaM.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentos.CSharp.Sobrecarga.Metodo
+{
+    internal class EstudoSobrecargaM
+    {
+        // definir o método contrutor da classe
+        public EstudoSobrecargaM() { }
+
+        // definir as descrições do método NomeDeAlguem(){}
+        // a tarefa que o método vai cumprir é: receber, como valor, o nome de uma pessoa e exibi-lo - a partir do uso de um objeto. A dinamica de funcionamento do método será baseada na sobrecarga de método
+        public string NomeDeAlguem(string PrimeiroNome)
+        {
+            // expressão de retorno do metodo
+            return $"Seu primeiro nome é {PrimeiroNome}";
+        }
+
+        // praticar a 1ª sobrecarga do método
+        public string NomeDeAlguem(string PrimeiroNome, string NomeDoMeio)
+        {
+            return $"Seus nomes, o primeiro e o nome do meio são {PrimeiroNome} e {NomeDoMeio}, respectivamente";
+        }
+
+        // 2ª sobrecarga do método
+        public string NomeDeAlguem(string PrimeiroNome, string NomeDoMeio, string TerceiroNome)
+        {
+            return $"Seu nome completo é {PrimeiroNome} {NomeDoMeio} {TerceiroNome}";
+        }
+    }
+}
---
+++ Fundamentos.CSharp.Sobrecarga.Metodo/Fundamentos.CSharp.Sobrecarga.Metodo/Program.cs
@@ -0,0 +1,15 @@
+﻿// praticar a instancia da classe EstudoSobrecargM{}
+using Fundamentos.CSharp.Sobrecarga.Metodo;
+
+EstudoSobrecargaM nome = new EstudoSobrecargaM();
+
+// fazer uso do objeto para chamar o método sobrecarregado à suas execuções
+Console.WriteLine(nome.NomeDeAlguem("Celio")); // 1ª chamada do método NomeDeAlguem()
+
+Console.WriteLine(new string('-', 50));
+
+Console.WriteLine(nome.NomeDeAlguem("Celio", "Soares")); // 2ª chamada do método sobrecarregado NomeDeAlguem()
+
+Console.WriteLine(new string('-', 50));
+
+Console.WriteLine(nome.NomeDeAlguem("Celio", "Soares", "De Souza"));
