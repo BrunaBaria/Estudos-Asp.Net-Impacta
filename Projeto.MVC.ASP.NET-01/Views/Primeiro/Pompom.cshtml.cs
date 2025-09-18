7bc5258b448192b435934bc183671285b3543281
BrunaBaria <136763236+BrunaBaria@users.noreply.github.com>
2025-09-18 17:49:06 -0300

    Add files via upload

---
+++ Projeto.MVC.ASP.NET-01/Views/Primeiro/Pompom.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Projeto.MVC.ASP.NET_01.Views.Primeiro
+{
+    public class PompomModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
